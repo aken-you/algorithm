9ab0558f7b17e1fec0fc9615375eafde90c3d8c7
aken-you <kinsk2839@naver.com>
2024-06-09 00:35:44 +0900

    [PGS] 구간 합 구하기 4 해답

--- baekjoon/구간 합 구하기 4.js
+++ baekjoon/구간 합 구하기 4.js
@@ -5,7 +5,7 @@
 // 합을 구해야 하는 횟수: m
 // 두번째 줄부터 i ~ j번째 수까지의 합
 
-// 시간 복잡도: O(n * m) =>
+// 시간 복잡도: O(n * m) => O(100000 * 100000) = O(100억) = 100초
 const fs = require("fs");
 let inputs = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 inputs = inputs.map((input) => input.trim().split(" ").map(Number));
@@ -31,3 +31,28 @@ for (let idx = 2; idx < 2 + m; idx++) {
 }
 
 console.log(result);
+
+// * 해답: 누적합
+// 시간복잡도: O(n + m)
+const fs = require("fs");
+let inputs = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+inputs = inputs.map((input) => input.trim().split(" ").map(Number));
+
+const n = inputs[0][0];
+const m = inputs[0][1];
+const arr = inputs[1];
+
+let result = "";
+const sum = new Array(n + 1).fill(0);
+
+// 누적합
+for (let idx = 1; idx <= arr.length; idx++) {
+  sum[idx] = sum[idx - 1] + arr[idx - 1];
+}
+
+for (let idx = 2; idx < m + 2; idx++) {
+  const [i, j] = inputs[idx];
+
+  if (result.length === 0) result += `${sum[j] - sum[i - 1]}`;
+  else result += `\n${sum[j] - sum[i - 1]}`;
+}
