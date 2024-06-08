c6193391c4c946548d64b3af25a996e5aeea719b
aken-you <kinsk2839@naver.com>
2024-06-08 21:51:00 +0900

    [PGS] 구간 합 구하기 4

---
+++ baekjoon/구간 합 구하기 4.js
@@ -0,0 +1,33 @@
+// https://www.acmicpc.net/problem/11659
+
+// * 내 풀이 - 시간 초과
+// 수의 개수: n
+// 합을 구해야 하는 횟수: m
+// 두번째 줄부터 i ~ j번째 수까지의 합
+
+// 시간 복잡도: O(n * m) =>
+const fs = require("fs");
+let inputs = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+inputs = inputs.map((input) => input.trim().split(" ").map(Number));
+
+const n = inputs[0][0];
+const m = inputs[0][1];
+
+let result = "";
+
+for (let idx = 2; idx < 2 + m; idx++) {
+  const [i, j] = inputs[idx];
+
+  let sum = 0;
+
+  inputs[1].forEach((e, idx) => {
+    if (idx >= i - 1 && idx <= j - 1) {
+      sum += e;
+    }
+  });
+
+  if (idx === 2) result += `${sum}`;
+  else result += `\n${sum}`;
+}
+
+console.log(result);
