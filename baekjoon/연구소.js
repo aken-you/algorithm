e7d617180de2eea0bf97867f0c6b819b589c0e48
aken-you <kinsk2839@naver.com>
2024-07-01 23:53:23 +0900

    [BJ] 연구소

--- baekjoon/연구소.js
+++ baekjoon/연구소.js
@@ -1,5 +1,6 @@
 // https://www.acmicpc.net/problem/14502
 // * 실패
+// * 1. bfs
 
 let inputs = require("fs").readFileSync("/dev/stdin").toString().trim();
 
@@ -78,6 +79,101 @@ for (let i = 0; i < empty.length - 2; i++) {
         max = cnt;
       }
     }
+  }
+}
+
+console.log(max);
+
+// * 2. dfs
+// 새로 세울 수 있는 벽 3개
+// 1: 벽
+// 2: 바이러스
+// 0: 바이러스 퍼질 수 있음
+
+// 바이러스가 퍼질 수 없는 곳: 안전 영역
+
+// return 안전 영역 크기 최댓값
+
+// 모든 영역 중에 3개를 골라서 벽을 세움
+// dfs를 돌린 다음, 0인 영역의 개수를 구함
+// 영역 개수 > max라면 max = 영역 개수
+
+let inputs = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+inputs = inputs.split("\n").map((e) => e.split(" ").map(Number));
+
+const [n, m] = inputs[0];
+
+const graph = inputs.slice(1);
+
+const dots = [];
+const virus = [];
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (graph[i][j] === 0) dots.push([i, j]);
+    else if (graph[i][j] === 2) virus.push([i, j]);
+  }
+}
+
+const cases = [];
+
+for (let i = 0; i < dots.length - 2; i++) {
+  for (let j = i + 1; j < dots.length - 1; j++) {
+    for (let k = j + 1; k < dots.length; k++) {
+      cases.push([i, j, k]);
+    }
+  }
+}
+
+const dx = [1, -1, 0, 0];
+const dy = [0, 0, 1, -1];
+
+const dotsCnt = dots.length;
+let max = 0;
+
+for (let i = 0; i < cases.length; i++) {
+  const [first, second, third] = cases[i];
+
+  let newGraph = graph.slice(0).map((arr) => [...arr]);
+
+  newGraph[dots[first][0]][dots[first][1]] = 1;
+  newGraph[dots[second][0]][dots[second][1]] = 1;
+  newGraph[dots[third][0]][dots[third][1]] = 1;
+
+  const checked = Array.from({ length: n }, () => Array(m).fill(false));
+  let cnt = 0;
+
+  // dfs
+  const dfs = (node) => {
+    const [x, y] = node;
+
+    if (!checked[x][y]) {
+      checked[x][y] = true;
+      newGraph[x][y] = 2;
+      cnt++;
+    }
+
+    for (let j = 0; j < 4; j++) {
+      const nx = x + dx[j];
+      const ny = y + dy[j];
+
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if (checked[nx][ny]) continue;
+      if (newGraph[nx][ny] === 1) continue;
+
+      dfs([nx, ny]);
+    }
+  };
+
+  for (let k = 0; k < virus.length; k++) {
+    dfs(virus[k]);
+  }
+
+  let result = dotsCnt - 3 - cnt + virus.length;
+
+  if (result > max) {
+    max = result;
   }
 }
 
