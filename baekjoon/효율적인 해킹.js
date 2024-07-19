2023c6e637ecd35a9d1104b1cfbb38d4ff73417b
aken-you <kinsk2839@naver.com>
2024-07-19 14:14:18 +0900

    [BJ] 효율적인 해킹

--- baekjoon/효율적인 해킹.js
+++ baekjoon/효율적인 해킹.js
@@ -64,7 +64,7 @@ for (let i = 1; i <= n; i++) {
 
 console.log(result.join(" "));
 
-// * 2. dfs
+// * 2. dfs - 스택
 
 const dfs = (graph, start) => {
   const stack = [start];
@@ -95,6 +95,60 @@ let result = [];
 
 for (let i = 1; i <= n; i++) {
   const cnt = dfs(graph, i);
+
+  if (cnt > max) {
+    max = cnt;
+    result = [i];
+  } else if (cnt === max) {
+    result.push(i);
+  }
+}
+
+console.log(result.join(" "));
+
+// * 3. dfs - 재귀
+let inputs = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+inputs = inputs.split("\n").map((input) => input.split(" ").map(Number));
+
+const [n, m] = inputs[0];
+inputs.shift();
+
+// graph 생성
+const graph = Array.from({ length: n + 1 }, (e) => []);
+
+inputs.forEach(([a, b]) => {
+  graph[b].push(a);
+});
+
+// dfs - 재귀
+// 모든 노드를 시작점으로 잡아서 탐색해야 할 듯..
+
+let checked = new Array(n + 1).fill(false);
+let cnt = 0;
+let max = 0;
+let result = [];
+
+const dfs = (node) => {
+  const next = graph[node];
+
+  for (let i = 0; i < next.length; i++) {
+    const adj = next[i];
+
+    if (!checked[adj]) {
+      cnt++;
+      checked[adj] = true;
+      dfs(adj);
+    }
+  }
+};
+
+for (let i = 1; i <= n; i++) {
+  cnt = 1;
+  checked.fill(false);
+
+  checked[i] = true;
+  dfs(i);
 
   if (cnt > max) {
     max = cnt;
