3e40ead4a48a2775422c5fe2c020823e5252a7f9
aken-you <kinsk2839@naver.com>
2025-02-11 14:51:19 +0900

    [PGS] 가장 큰 수

--- programmers/lv2/가장 큰 수.js
+++ programmers/lv2/가장 큰 수.js
@@ -1,6 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42746#
 // !실패
 
+// 3, 30이 있을 경우
+// 303 < 330이므로 3이 앞에 있어야 한다.
+
 function solution(numbers) {
   numbers.sort((a, b) => {
     let A = String(a);
@@ -11,11 +14,13 @@ function solution(numbers) {
     const aHead = +A[0];
     const bHead = +B[0];
 
+    // 맨 앞자리가 다를 경우
     if (aHead > bHead) return -1;
     else if (aHead < bHead) return 1;
 
-    const combine1 = +(A + B);
-    const combine2 = +(B + A);
+    // 맨 앞자리가 같을 경우
+    const combine1 = +(A + B); // 303
+    const combine2 = +(B + A); // 330
 
     return combine2 - combine1;
   });
@@ -30,3 +35,15 @@ function solution(numbers) {
 
 // arr.sort((a, b) => a - b) => 오름차순
 // a > b일 경우에만 a와 b의 자리를 바꾼다.
+
+// ! 실패
+function solution(numbers) {
+  const result = numbers
+    .map(String)
+    .sort((a, b) => {
+      return b + a - (a + b);
+    })
+    .join("");
+
+  return result[0] === "0" ? "0" : result;
+}
